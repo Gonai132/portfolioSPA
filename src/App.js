829f9331ea3b2a7417ce36a8e12039ffdd9d9c01
Gonai <margaret.szymanik@gmail.com>
2024-12-21 21:40:58 +0100

    restyling navbar section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
+import Home from "./components/Home/Home";
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Home></Home>
     </div>
     
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./home.css"
+
+const Home = () => {
+  return (
+    <div className='card'>Home</div>
+  )
+}
+
+export default Home
